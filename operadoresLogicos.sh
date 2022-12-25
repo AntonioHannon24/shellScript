69b5c1d9fea348829751414470dc6606e6b9a3d1
Antonio <antoniohannon24@gmail.com>
2022-12-24 21:16:47 -0300

    confirmar

---
+++ operadoresLogicos.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+nomeConferir="Antonio"
+ 
+read -p "Qual é seu nome? " nome
+test -z "$nome"                         \
+&& { echo "O campo nome está nulo"; exit; } \
+|| echo "Sua Variavel não é nula"
+
+test "$nome" = "$nomeConferir" \
+&& echo "$nome é igual a $nomeConferir" \
+|| echo "$nome é diferente de $nomeConferir" 
+
+
+
+
+
