ee74a2bdc01d4247928ff199588bf6dbf31a0735
Antonio <antoniohannon24@gmail.com>
2022-12-27 20:24:57 -0300

    confirmar

--- menu.sh
+++ menu.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
-
-
 echo "==========================================="
 echo "1) Abra o Mozzila"
 echo "2) Abra o Gedit"
 echo "3) Abra o VsCode"
 echo "4) Abra o Postman"
+echo "5) Sair"
 echo "==========================================="
 read -p "Selecione uma das opções: " opcao
 
 if [[ "$opcao" -eq "1" ]]; then
-    firefox
+    firefox &
+    ./menu.sh
 elif [[ "$opcao" -eq "2" ]];then
-    gedit
+    gedit &
+    ./menu.sh
 elif [[ "$opcao" -eq "3" ]];then
-    code
+    code &
+    ./menu.sh
 elif [[ "$opcao" -eq "4" ]];then
-    postman
+    postman &
+    ./menu.sh
+elif [[ "$opcao" -eq "5" ]];then
+    echo "Saindo do programa!"; exit 0;
 else
     echo "Código digitado incorretamente!!"
-    exit 1
+    ./menu.sh
 fi
