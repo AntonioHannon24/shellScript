f211497a0fbb00d9d0b10d5b190ec5688347ba8c
Antonio <antoniohannon24@gmail.com>
2022-12-27 17:31:21 -0300

    confirmar

--- menu.sh
+++ menu.sh
@@ -6,20 +6,19 @@ echo "==========================================="
 echo "1) Abra o Mozzila"
 echo "2) Abra o Gedit"
 echo "3) Abra o VsCode"
+echo "4) Abra o Postman"
 echo "==========================================="
 read -p "Selecione uma das opções: " opcao
 
 if [[ "$opcao" -eq "1" ]]; then
-
     firefox
 elif [[ "$opcao" -eq "2" ]];then
-
     gedit
 elif [[ "$opcao" -eq "3" ]];then
-
     code
+elif [[ "$opcao" -eq "4" ]];then
+    postman
 else
-
     echo "Código digitado incorretamente!!"
     exit 1
 fi
